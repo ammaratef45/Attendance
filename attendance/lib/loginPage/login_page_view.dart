175d15ddcd429f50149ed7a63c2ca5fc51893380
Bahaaib <Bahaasco@gmail.com>
2019-03-31 11:57:32 +0200

    MOD: Login page

--- attendance/lib/loginPage/login_page_view.dart
+++ attendance/lib/loginPage/login_page_view.dart
@@ -1,33 +1,92 @@
+import 'package:attendance/anim//fade_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 import './login_page_viewmodel.dart';
 
 class LoginPageView extends LoginPageViewModel {
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Login"),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              child: Text("Sign In"),
-              onPressed: ()=>signInUser().then((FirebaseUser user)=>handleUser(user)),
-              color: Colors.green,
-            ),
-            Text("Or"),
-            RaisedButton(
-              child: Text("Offline mode"),
-              onPressed: ()=>Navigator.of(context).pushNamed('/offline'),
-              color: Colors.green,
-            ),
-          ],
-        ),
-      ),
-    );
+        backgroundColor: Colors.lightBlueAccent,
+        body: SafeArea(
+            child: ListView(
+              children: <Widget>[
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+                    Widget>[
+                  Container(
+                    child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        FadeIn(
+                          child: Text(
+                            "Welcome,",
+                            style: TextStyle(
+                                color: Colors.white, fontSize: 30.0),
+                          ),
+                          delay: 1000,
+                        ), FadeIn(
+                          child: Container(child: Text(
+                            "Let's get you in..",
+                            style: TextStyle(
+                                color: Colors.white, fontSize: 35.0),
+                          ), padding: EdgeInsets.only(top: 25),),
+                          delay: 1500,
+                        )
+                      ],
+                    ),
+                    padding: EdgeInsets.only(left: 15, top: 130),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(top: 120),
+                      child: Center(
+                        child: new RaisedButton(
+                            padding:
+                            EdgeInsets.only(top: 3.0, bottom: 3.0, left: 6.0),
+                            color: const Color(0xFFFFFFFF),
+                            onPressed: () =>
+                                signInUser().then((FirebaseUser user) =>
+                                    handleUser(user)),
+                            child: new Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                new Image.asset(
+                                  'assets/icons/g-logo.png',
+                                  fit: BoxFit.fill,
+                                  height: 40.0,
+                                ),
+                                new Container(
+                                    padding:
+                                    EdgeInsets.only(left: 15.0, right: 10.0),
+                                    child: new Text(
+                                      "Sign in with Google",
+                                      style: TextStyle(
+                                          color: Colors.black38,
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                              ],
+                            )),
+                      )),
+                  Container(
+                      margin: EdgeInsets.only(top: 20.0),
+                      child: Center(
+                        child: Text(
+                          "Having troubles?",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      )),
+                  Container(
+                    margin: EdgeInsets.only(top: 20.0),
+                    child: Center(
+                      child: OutlineButton(
+                          child: new Text("Go offline"),
+                          onPressed: () =>
+                              Navigator.of(context).pushNamed('/offline'),
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(30.0))),
+                    ),
+                  )
+                ])
+              ],
+            )));
   }
 }

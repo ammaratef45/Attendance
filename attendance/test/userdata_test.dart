94d793470419fcce6271a3c811433306928c8f5b
Ammar Atef <a_atef45@yahoo.com>
2019-04-14 11:34:41 +0200

    #43 solved

--- attendance/test/userdata_test.dart
+++ attendance/test/userdata_test.dart
@@ -1,24 +1,27 @@
+import 'dart:convert';
+
 import 'package:attendance/backend/user.dart';
 import 'package:test/test.dart';
 
 void main() {
-  User testUser = User.instance();
-  String perfectName = 'someName';
-  String perfectPhone = '1234567890';
+  final User _testUser = User.instance();
+  const String _perfectName = 'someName';
+  const String _perfectPhone = '1234567890';
 
-  // @todo #42 Use Map object to create a testing json body
-  String jsonBody =
-      '{' + '"nativeName":"$perfectName",' + '"phone":"$perfectPhone"' + '}';
+  final Map<String, dynamic> data = <String, dynamic>{
+    'nativeName': _perfectName,
+    'phone': _perfectPhone
+  };
 
 
   group('object inistantiation', () {
     test('object should not be null', () {
-      expect(testUser, isNotNull);
+      expect(_testUser, isNotNull);
     });
 
     test('singleton should not leak ', () {
-      User instance1 = new User();
-      User instance2 = new User();
+      final User instance1 = User();
+      final User instance2 = User();
 
       expect(instance1.hashCode, equals(instance2.hashCode));
     });
@@ -26,100 +29,100 @@ void main() {
 
   group('Rename', () {
     test('should has no default values', () {
-      expect(testUser.nativeName, isNull);
+      expect(_testUser.nativeName, isNull);
     });
 
     test('should accept empty string', () {
-      testUser.rename('');
+      _testUser.rename('');
 
-      expect(testUser.nativeName, isEmpty);
+      expect(_testUser.nativeName, isEmpty);
     });
 
     test('should be matched ', () {
-      testUser.rename(perfectName);
+      _testUser.rename(_perfectName);
 
-      expect(testUser.nativeName, 'someName');
+      expect(_testUser.nativeName, 'someName');
     });
 
     test('should reject names longer than 20 chars', () {
       try {
-        testUser.rename('This is a very long name');
+        _testUser.rename('This is a very long name');
       } on FormatException catch (ex) {
         expect(ex.message, 'invalid native name format');
       }
     });
 
     test('should accept names shorter than 21 chars', () {
-      testUser.rename(perfectName);
+      _testUser.rename(_perfectName);
 
-      expect(testUser.nativeName, perfectName);
+      expect(_testUser.nativeName, _perfectName);
     });
   });
 
   group('changePhone', () {
     test('should has no default values', () {
-      expect(testUser.phone, isNull);
+      expect(_testUser.phone, isNull);
     });
     test('should accept empty string', () {
-      testUser.changePhone('');
+      _testUser.changePhone('');
 
-      expect(testUser.phone, isEmpty);
+      expect(_testUser.phone, isEmpty);
     });
 
     test('should reject numbers start with leading zero ', () {
       try {
-        testUser.changePhone('0123456789');
+        _testUser.changePhone('0123456789');
       } on FormatException catch (ex) {
         expect(ex.message, 'invalid phone number format');
       }
     });
 
     test('should accept numbers start with non-leading zero ', () {
-      testUser.changePhone(perfectPhone);
+      _testUser.changePhone(_perfectPhone);
 
-      expect(testUser.phone, perfectPhone);
+      expect(_testUser.phone, _perfectPhone);
     });
 
     test('should reject numbers start with country code ', () {
       try {
-        testUser.changePhone('+012345678');
+        _testUser.changePhone('+012345678');
       } on FormatException catch (ex) {
         expect(ex.message, 'invalid phone number format');
       }
     });
 
     test('should accept numbers start with no country code ', () {
-      testUser.changePhone(perfectPhone);
+      _testUser.changePhone(_perfectPhone);
 
-      expect(testUser.phone, perfectPhone);
+      expect(_testUser.phone, _perfectPhone);
     });
 
     test('should be matched ', () {
-      expect(testUser.phone, '1234567890');
+      expect(_testUser.phone, '1234567890');
     });
 
     test('should reject numbers longer than 10 chars', () {
       try {
-        testUser.changePhone('01234567890123456789');
+        _testUser.changePhone('154234567890123456789');
       } on FormatException catch (ex) {
         expect(ex.message, 'invalid phone number format');
       }
     });
 
     test('should accept numbers shorter than 11 chars', () {
-      testUser.changePhone(perfectPhone);
+      _testUser.changePhone(_perfectPhone);
 
-      expect(testUser.phone, perfectPhone);
+      expect(_testUser.phone, _perfectPhone);
     });
   });
 
   group('requestBody', () {
     test('should return non empty body', () {
-      expect(testUser.requestBody(), isNotEmpty);
+      expect(_testUser.requestBody(), isNotEmpty);
     });
 
     test('should return valid body', () {
-      expect(testUser.requestBody(), jsonBody);
+      expect(_testUser.requestBody(), json.encode(data));
     });
   });
 }

08e7465a34e5d134a855b622a7383d4ada7e8f5b
Ammar Atef <ammar.atef45@gmail.com>
2019-04-18 14:03:39 +0200

    Update cleanENV.sh

--- attendance/cleanENV.sh
+++ attendance/cleanENV.sh
@@ -3,7 +3,7 @@ if [[ ! -e adminsdk.json ]]; then
     touch adminsdk.json
     echo $ADMIN_SDK > adminsdk.json
 fi
-#! /bin/bash -
+
 if [[ ! -e firebase_token_generator.py ]]; then
     touch firebase_token_generator.py
     echo $TOKEN_GENERATOR > firebase_token_generator.py

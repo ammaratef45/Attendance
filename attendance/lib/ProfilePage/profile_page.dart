9f70988c4e6b8670c392dbe18891dc0eb5793f6b
Bahaaib <Bahaasco@gmail.com>
2019-04-24 19:34:41 +0200

    cleaned Dart warnings

--- attendance/lib/ProfilePage/profile_page.dart
+++ attendance/lib/ProfilePage/profile_page.dart
@@ -1,8 +1,10 @@
 import 'package:attendance/ProfilePage/profile_page_view.dart';
 import 'package:flutter/material.dart';
 
+///Profile page
 class ProfilePage extends StatefulWidget {
-  ProfilePage({Key key}) : super(key: key);
+  ///Profile page default constructor
+  const ProfilePage({Key key}) : super(key: key);
 
   @override
   ProfilePageView createState() => ProfilePageView();

f61cbeb3db840df24c67f95964f7bbecac482efa
Bahaaib <Bahaasco@gmail.com>
2019-04-27 23:17:16 +0200

    added scan FAB

--- attendance/lib/homePage/home_page_view.dart
+++ attendance/lib/homePage/home_page_view.dart
@@ -1,15 +1,12 @@
-import 'package:attendance/homePage/home_page_viewmodel.dart';
 import 'package:attendance/backend/attend_model.dart';
+import 'package:attendance/homePage/home_page_viewmodel.dart';
 import 'package:flutter/material.dart';
 
 /// The view of the home page
 class HomePageView extends HomePageViewModel {
   /// build the body of the page
-  Widget buildBody(
-    BuildContext ctxt,
-    int index,
-    {bool filterForUnLeaved=false}
-  ) {
+  Widget buildBody(BuildContext ctxt, int index,
+      {bool filterForUnLeaved = false}) {
     final AttendModel model = filterForUnLeaved ? uitems[index] : litems[index];
     final Widget result = Center(
       child: Card(
@@ -42,39 +39,29 @@ class HomePageView extends HomePageViewModel {
 
   /// get the tabs
   List<Tab> tabs() {
-    final List<Tab> tabs = <Tab>[]
-    ..add(const Tab(icon: Icon(Icons.access_time)))
-    ..add(const Tab(icon: Icon(Icons.all_inclusive)))
-    ..add(const Tab(icon: Icon(Icons.add_circle)));
+    final List<Tab> tabs = <Tab>[]..add(
+        const Tab(icon: Icon(Icons.access_time)))..add(
+        const Tab(icon: Icon(Icons.all_inclusive)))..add(
+        const Tab(icon: Icon(Icons.add_circle)));
     return tabs;
   }
 
   /// get the items of the body
   List<Widget> bodyItems() {
-    final List<Widget> widgs = <Widget>[]
-    ..add(
-      ListView.builder(
+    final List<Widget> widgs = <Widget>[]..add(ListView.builder(
         itemCount: uitems.length,
         itemBuilder: (BuildContext ctxt, int index) =>
-          buildBody(ctxt, index, filterForUnLeaved: true)
-      )
-    )
-    ..add(
-      ListView.builder(
-        itemCount: litems.length,
-        itemBuilder: (BuildContext ctxt, int index) =>
-          buildBody(ctxt, index, filterForUnLeaved: false)
-      )
-    )
-    ..add(
-      Center(
+            buildBody(ctxt, index, filterForUnLeaved: true)))..add(
+        ListView.builder(
+            itemCount: litems.length,
+            itemBuilder: (BuildContext ctxt, int index) =>
+                buildBody(ctxt, index, filterForUnLeaved: false)))..add(Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 16, vertical: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               child: RaisedButton(
                   color: Colors.blue,
                   textColor: Colors.white,
@@ -83,8 +70,7 @@ class HomePageView extends HomePageViewModel {
                   child: const Text('START CAMERA SCAN')),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 16, vertical: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               child: Text(
                 scanResult,
                 textAlign: TextAlign.center,
@@ -92,63 +78,65 @@ class HomePageView extends HomePageViewModel {
             ),
           ],
         ),
-      )
-    );
+    ));
     return widgs;
   }
 
   @override
   Widget build(BuildContext context) =>
-    DefaultTabController(
-        length: 3,
-        child: Scaffold(
-            appBar: AppBar(
-              bottom: TabBar(
-                tabs: tabs(),
+      DefaultTabController(
+          length: 3,
+          child: Scaffold(
+              floatingActionButton: FloatingActionButton.extended(
+                  onPressed: () {},
+                  icon: const Icon(Icons.camera_alt),
+                  label: const Text('Scan')),
+              appBar: AppBar(
+                bottom: TabBar(
+                  tabs: tabs(),
+                ),
+                title: const Text('Home'),
               ),
-              title: const Text('Home'),
-            ),
-            drawer: Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  UserAccountsDrawerHeader(
-                    accountName: Text(name),
-                    accountEmail: Text(mail),
-                    currentAccountPicture: CircleAvatar(
-                      backgroundImage: NetworkImage(imageUrl),
+              drawer: Drawer(
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: <Widget>[
+                    UserAccountsDrawerHeader(
+                      accountName: Text(name),
+                      accountEmail: Text(mail),
+                      currentAccountPicture: CircleAvatar(
+                        backgroundImage: NetworkImage(imageUrl),
+                      ),
                     ),
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.cloud_off),
-                    title: const Text('Offline scan'),
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.of(context).pushNamed('/offline');
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.account_circle),
-                    title: const Text('Profile'),
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.of(context).pushNamed('/profile');
-                    },
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: const Icon(Icons.arrow_back),
-                    title: const Text('Sign out'),
-                    onTap: () {
-                      Navigator.pop(context);
-                      signOut();
-                    },
-                  )
-                ],
+                    ListTile(
+                      leading: const Icon(Icons.cloud_off),
+                      title: const Text('Offline scan'),
+                      onTap: () {
+                        Navigator.pop(context);
+                        Navigator.of(context).pushNamed('/offline');
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.account_circle),
+                      title: const Text('Profile'),
+                      onTap: () {
+                        Navigator.pop(context);
+                        Navigator.of(context).pushNamed('/profile');
+                      },
+                    ),
+                    Divider(),
+                    ListTile(
+                      leading: const Icon(Icons.arrow_back),
+                      title: const Text('Sign out'),
+                      onTap: () {
+                        Navigator.pop(context);
+                        signOut();
+                      },
+                    )
+                  ],
+                ),
               ),
-            ),
-            body: TabBarView(
-              children: bodyItems(),
-            )));
-
+              body: TabBarView(
+                children: bodyItems(),
+              )));
 }

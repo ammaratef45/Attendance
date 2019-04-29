ff6056396d0a51d2a70a69adc44e39c4f53df16c
Ammar Atef <a_atef45@yahoo.com>
2019-04-29 12:58:12 +0200

    fixed script

--- attendance/cleanENV.sh
+++ attendance/cleanENV.sh
@@ -3,7 +3,7 @@ if [[ ! -e adminsdk.json ]]; then
     touch adminsdk.json
     echo $ADMIN_SDK > adminsdk.json
 fi
-if [[ ! -e exportEnvVars.sh ]]; then
+if [[ -e exportEnvVars.sh ]]; then
     chmod +x exportEnvVars.sh
     ./exportEnvVars.sh
 fi

72221ae5327e4113e8f8a764aa228ea36a181fb0
Ammar Atef <a_atef45@yahoo.com>
2019-05-01 12:18:19 +0200

    #76 renamed the model attendance

--- attendance/lib/homePage/home_page_view.dart
+++ attendance/lib/homePage/home_page_view.dart
@@ -1,4 +1,4 @@
-import 'package:attendance/backend/attend_model.dart';
+import 'package:attendance/backend/attendance.dart';
 import 'package:attendance/homePage/home_page_viewmodel.dart';
 import 'package:flutter/material.dart';
 

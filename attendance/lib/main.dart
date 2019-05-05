bd7781c10d5a4c1dd606a18e67e8d09d089d85a1
Ammar Atef <a_atef45@yahoo.com>
2019-05-05 08:03:18 +0200

    #79 refactored main file for lint fixing

--- attendance/lib/main.dart
+++ attendance/lib/main.dart
@@ -1,31 +1,32 @@
+import 'package:attendance/AttendanceDatailsPage/attendance_details_page.dart';
+import 'package:attendance/ProfilePage/profile_page.dart';
+import 'package:attendance/homePage/home_page.dart';
+import 'package:attendance/loginPage/login_page.dart';
+import 'package:attendance/offline_page/offline_page.dart';
 import 'package:flutter/material.dart';
 
-import './AttendanceDatailsPage/attendance_details_page.dart';
-import './ProfilePage/profile_page.dart';
-import './homePage/home_page.dart';
-import './loginPage/login_page.dart';
-import './offline_page/offline_page.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
+/// The Application class
 class MyApp extends StatelessWidget {
-  final Widget myHome = LoginPage();
+  final Widget _myHome = LoginPage();
 
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
+  Widget build(BuildContext context) =>
+    MaterialApp(
       title: 'Attendance',
-      theme: new ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: myHome,
+      home: _myHome,
       routes: <String, WidgetBuilder>{
-        '/login': (BuildContext context) => new LoginPage(),
-        '/home': (BuildContext context) => new HomePage(),
-        '/details': (BuildContext context) => new AttendanceDetailsPage(),
-        '/offline': (BuildContext context) => new OfflinePage(),
-        '/profile': (BuildContext context) => new ProfilePage()
+        '/login': (BuildContext context) => LoginPage(),
+        '/home': (BuildContext context) => HomePage(),
+        '/details': (BuildContext context) => const AttendanceDetailsPage(),
+        '/offline': (BuildContext context) => OfflinePage(),
+        '/profile': (BuildContext context) => const ProfilePage()
       },
     );
-  }
+
 }
